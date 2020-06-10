5bdc7acee6fc8511d0ec500982b44c561dc4fb98
Davidoffili <palagashvilid@gmail.com>
2020-06-09 23:11:23 -0700

    finished fetch function, note object is returned showing KVPs

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict';
 
-//TODO: Require the library files you will be writing (input, notes).
+// Require the library files we will be writing.
 const Input = require('./lib/input');
 const Note = require('./lib/notes');
 
-//TODO: Instantiate an instance of an “Input” parser module
+// Instantiate an instance of the “Input” parser module.
 const parsed = new Input();
-console.log(parsed);
+
 
 //TODO: Pass the command to the Notes library, which executes the command.
+function fetch(parsed) {
+    if (parsed.action === false) {
+        console.log('Something went wrong with your action, try again!')
+    } else {
+        console.log(`Successfully added note: ${parsed.payload}`);
+    }
+}
+fetch(parsed);
+
+let userNote = new Note(parsed);
+console.log(userNote);
